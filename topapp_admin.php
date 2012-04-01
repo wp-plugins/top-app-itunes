6f9517844a1505bd215bee903680ff6673ab9c92
jbma <jbma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-01 16:58:58 +0000

    git-svn-id: https://plugins.svn.wordpress.org/top-app-itunes/trunk@525833 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- topapp_admin.php
+++ topapp_admin.php
@@ -22,7 +22,7 @@
   		  unlink($cache_file);
   		       
   		        ?>  
-        <div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>  
+        <div class="updated"><p><strong><?php _e('Options saved.','topapp' ); ?></strong></p></div>  
         <?php  
     									} 
     
@@ -40,7 +40,7 @@
 
 ?> 
 <div class="wrap">   
-<?php    echo "<h2>" . __( 'Top App Configuration', 'topapp_trdom' ) . "</h2>"; ?>  
+<?php echo "<h2>" . __( 'Top App Configuration', 'topapp' ) . "</h2>"; ?>  
 
 <div id="poststuff" class="metabox-holder">
 
@@ -53,18 +53,18 @@
 	    <form name="topapp_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
         <input type="hidden" name="topapp_hidden" id="topapp_hidden" value="Y">  
        <tr>
-       <th> <?php _e("Lien d'affiliation : " ); ?></th><td><input type="text" name="topapp_affi" id="topapp_affi" value="<?php echo $affi_config; ?>" size="20"> <br /><span class="description">Mettez votre lien d'affiliation sous la forme : http://clk.tradedoubler.com/click?p=23753&a=2076412&url=</span></td></tr>
+       <th> <?php _e("Lien d'affiliation : ",'topapp' ); ?></th><td><input type="text" name="topapp_affi" id="topapp_affi" value="<?php echo $affi_config; ?>" size="20"> <br /><span class="description"><?php _e("Mettez votre lien d'affiliation sous la forme : http://clk.tradedoubler.com/click?p=23753&a=2076412&url=",'topapp');?></span></td></tr>
        <tr> 
-       <th><?php _e("Top à afficher : " ); ?></th><td><select name="topapp_cat"> <option>iPhone</option><option>iPad</option>   </select><br/> <span class="description">Sélection du Top à afficher : <strong><?php echo $cat_config;?></strong</span></td>  </tr>  
+       <th><?php _e("Top à afficher : ",'topapp' ); ?></th><td><select name="topapp_cat"> <option>iPhone</option><option>iPad</option>   </select><br/> <span class="description"><?php _e("Sélection du Top à afficher : ","topapp");?><strong><?php echo $cat_config;?></strong</span></td>  </tr>  
      <tr>
-     <th><?php _e("Nous supporter : " ); ?></th><td><select name="topapp_soutien"><option>Oui</option><option>Non</option> </select>
-     <br/> <span class="description"> Affiche un lien sous le widget : <strong><?php echo $soutien;?></strong</span></td></tr>
+     <th><?php _e("Nous supporter : ",'topapp' ); ?></th><td><select name="topapp_soutien"><option><?php _e("Oui","topapp");?></option><option><?php _e("Non","topapp");?></option> </select>
+     <br/> <span class="description"><?php _e(" Affiche un lien sous le widget : ","topapp");?><strong><?php echo $soutien;?></strong</span></td></tr>
 
   </table>
         
          
         <p class="submit">  
-        <input type="submit" name="Submit" value="<?php _e('Mettre à jour les options', 'topapp_trdom' ) ?>" />  
+        <input type="submit" name="Submit" value="<?php _e('Mettre à jour les options', 'topapp' ) ?>" />  
         </p>  
     </form>  
   
